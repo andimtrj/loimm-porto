a25a2f1c6f7e3bf0a604d9e74aa3251f85d0746b
Andi Mataraja <amataraja@gmail.com>
2025-04-15 20:41:20 +0700

    NewHome Page responsive done

--- src/pages/NewHome.jsx
+++ src/pages/NewHome.jsx
@@ -19,29 +19,39 @@ const h1Animation2 = {
 function NewHome() {
   return (
     <div className="flex justify-between overflow-hidden">
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col lg:xl:justify-between w-full">
         <Navbar />
-        <div className="pb-5 pl-3">
-          <div style={{ clipPath: "inset(0 0 0 0)" }} className="w-fit">
-            <motion.h1
-              {...h1Animation}
-              className="text-6xl font-extrabold tracking-tighter"
-            >
-              HELLO! I'M CARLO - A HUMAN
-            </motion.h1>
+
+        <div className="sm:flex sm:flex-col sm:justify-center sm:items-center">
+          <div className="overflow-hidden block flex-shrink-0 w-full h-[80vh] lg:xl:hidden px-5 mb-2">
+            <img
+              src={aboutFoto}
+              alt=""
+              className="w-full h-full object-cover"
+            />
           </div>
-          <div style={{ clipPath: "inset(0 0 0 0)" }} className="w-fit">
-            <motion.h1
-              {...h1Animation2}
-              className="text-6xl font-extrabold tracking-tighter"
-            >
-              WHO MAKES PICTURES TALK :)
-            </motion.h1>
+          <div className="pb-5 lg:xl:pl-5 sm:pl-0 text-2xl lg:text-5xl xl:text-6xl w-full lg:xl:block flex flex-col justify-center items-center">
+            <div style={{ clipPath: "inset(0 0 0 0)" }} className="w-fit">
+              <motion.h1
+                {...h1Animation}
+                className="font-extrabold tracking-tighter"
+              >
+                HELLO! I'M CARLO - A HUMAN
+              </motion.h1>
+            </div>
+            <div style={{ clipPath: "inset(0 0 0 0)" }} className="w-fit">
+              <motion.h1
+                {...h1Animation2}
+                className="font-extrabold tracking-tighter"
+              >
+                WHO MAKES PICTURES TALK :)
+              </motion.h1>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="overflow-hidden block flex-shrink-0 w-[40vw] h-screen md:w-[35vw]">
+      <div className="overflow-hidden lg:xl:block flex-shrink-0 w-[40vw] h-screen sm:hidden">
         <img src={aboutFoto} alt="" className="w-full h-full object-cover" />
       </div>
     </div>
